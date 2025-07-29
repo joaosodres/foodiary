9575e732dc2b5ab320df5ccad4fee646813ae4c2
joaosodres <joaov1070@gmail.com>
2025-07-29 14:51:08 -0300

    processo de salvar usuario no banco de dados

--- api-foodiary/src/controllers/SignUpController.ts
+++ api-foodiary/src/controllers/SignUpController.ts
@@ -1,7 +1,10 @@
 import z from "zod";
 
 import { HttpRequest, HttpResponse } from "../types/Http";
-import { badRequest, created } from "../utils/http";
+import { badRequest, conflict, created } from "../utils/http";
+import { db } from "../db";
+import { usersTable } from "../db/schema";
+import { eq } from "drizzle-orm";
 
 const schema = z.object({
   goal: z.enum(["lose", "maintain", "gain"]),
@@ -25,7 +28,35 @@ export class SignUpController {
       return badRequest({errors: error.issues});
     }
 
+    const userAlreadyExists = await db.query.usersTable.findFirst({
+      columns: {
+        email: true,
+      },
+      where: eq(usersTable.email, data.account.email),
+    });
 
-    return created({data});
+    if (userAlreadyExists) {
+      return conflict({error: 'User already exists'});
+    }
+
+    const  {account, ...rest} = data;
+
+    const [user] = await db
+    .insert(usersTable)
+    .values({
+      ...rest,
+      ...account,
+      calories: 0,
+      proteins: 0,
+      carbs: 0,
+      fats: 0,
+    })
+    .returning({
+      id: usersTable.id,
+    });
+
+    return created({
+      userId: user.id,
+    });
   }
 }
