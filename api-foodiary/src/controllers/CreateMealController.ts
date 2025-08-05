b2c453e4a21259b17a09e683c4e3f437bf1da407
joaosodres <joaov1070@gmail.com>
2025-08-05 10:33:40 -0300

    upload para o s3

--- api-foodiary/src/controllers/CreateMealController.ts
+++ api-foodiary/src/controllers/CreateMealController.ts
@@ -4,6 +4,10 @@ import { mealsTable, usersTable } from "../db/schema";
 import { ProtectedHttpRequest, HttpResponse } from "../types/Http";
 import { badRequest, created, ok, unauthorized } from "../utils/http";
 import z from "zod";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { randomUUID } from "crypto";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { s3Client } from "../clients/s3Client";
 
 const schema = z.object({
   fileType: z.enum(["audio/m4a", "image/jpeg"]),
@@ -16,6 +20,19 @@ export class CreateMealController {
     if (!success) {
       return badRequest({errors: error.issues});
     }
+
+    const fileId = randomUUID();
+    const ext = data.fileType === 'audio/m4a' ? '.m4a' : '.jpg';
+    const fileKey = `${fileId}${ext}`;
+
+    const command = new PutObjectCommand({
+      Bucket: process.env.BUCKET_NAME,
+      Key: fileKey
+    });
+
+    const presignedUrl = await getSignedUrl(s3Client, command, {
+      expiresIn: 600,
+    });
 
     const [meal] = await db
     .insert(mealsTable)
@@ -30,6 +47,9 @@ export class CreateMealController {
     })
     .returning({id: mealsTable.id});
 
-    return created({ mealId: meal.id });
+    return created({
+      mealId: meal.id,
+      uploadUrl: presignedUrl
+     });
   }
 }
